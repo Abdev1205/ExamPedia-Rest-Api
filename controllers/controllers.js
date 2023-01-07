00b748b2d443d9855152a78768541b6e7a61ec88
Abhay1205Mishra <97285855+Abhay1205Mishra@users.noreply.github.com>
2023-01-08 00:40:45 +0530

    env set

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,9 +1,9 @@
-
+require("dotenv").config();
 const {google} = require('googleapis')
-const CLIENT_ID = '283287698711-9a3q4taur5k5jrf1usfbf8j61peuig1i.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//0glb8ZHUbOXM3CgYIARAAGBASNwF-L9IrwCoB46s1NtGgQ9I3iPnd2z7nv8s4PvzaJNc7bUtmf1r1qFajosmg56IQHckrAZaSKc0';
+const CLIENT_ID = process.env.CLIENT_ID ;
+const CLIENT_SECRET = process.env.CLIENT_SECRET ;
+const REDIRECT_URI = process.env.REDIRECT_URI ;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const fs = require('fs')
 const note = require('../models/note')
 const pyq = require('../models/pyq')
